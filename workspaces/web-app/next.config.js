2a1c9f60a37b0d318bfc6a7d432e318a4d890b74
Marcin Czenko <marcin@everydayproductive.com>
2019-02-08 17:00:59 +0100

    Removes publicRuntimeConfig from next.config.js - invalid for serverless deployment

--- workspaces/web-app/next.config.js
+++ workspaces/web-app/next.config.js
@@ -3,9 +3,6 @@ const webpack = require('webpack')
 
 module.exports = withCSS({
   target: 'serverless',
-  publicRuntimeConfig: {
-    firebasePwd: process.env.FIREBASE_PWD
-  },
   webpack (config) {
     config.module.rules.push({
       test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
