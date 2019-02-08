b7418fde402c7bd11321cfdbb4e96592c65a6fe5
Marcin Czenko <marcin@everydayproductive.com>
2019-02-08 16:54:55 +0100

    Creating assignments in Firestore

--- workspaces/web-app/components/home/Home.js
+++ workspaces/web-app/components/home/Home.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react'
 import { FadingValueBox } from '../animations'
 import { AssignmentDialog } from '../assignments'
 
+const getCurrentlySignedUser = () => {
+  const user = firebase.auth().currentUser
+  if (user) {
+    return user.uid
+  } else {
+    return null
+  }
+}
+
+const createAssignment = async assignment => {
+  try {
+    const uid = getCurrentlySignedUser()
+    if (uid) {
+      const { assignmentName, projectNumber } = assignment
+      const db = firebase.firestore()
+      await db.collection('assignments').add({
+        assignmentName,
+        projectNumber
+      })
+    } else {
+      throw new Error('Having trouble accesing Firebase. Please try again...')
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+// const read = async tag => {
+//   const uid = getCurrentlySignedUser()
+//   if (uid) {
+//     const db = firebase.firestore()
+//     const doc = await db.collection('users').doc(uid).get()
+//     const senderTag = doc.get(tag)
+
+//   } else {
+//     throw new Error('Having trouble accesing Firebase. Please try again...')
+//   }
+// }
 class Home extends Component {
   state = {
     open: false
   }
 
-  addAssignment = () => {
-    console.log('adding assignment')
+  addAssignment = async assignment => {
+    this.setState({ open: false })
+    await createAssignment(assignment)
   }
 
   openAssignmentDialog = () => {
