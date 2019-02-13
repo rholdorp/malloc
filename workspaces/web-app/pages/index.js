1de79acf640a8a09a4c00548d23340400afb351e
Marcin Czenko <marcin@everydayproductive.com>
2019-02-13 18:07:40 +0100

    Fixes title and the default text color

--- workspaces/web-app/pages/index.js
+++ workspaces/web-app/pages/index.js
@@ -3,7 +3,7 @@ import { PageCentered } from '@react-frontend-developer/react-layout-helpers'
 import { Home } from '../components/home'
 
 const Index = () => (
-  <PageCentered css={{ color: 'white' }}>
+  <PageCentered>
     <div css={{ display: 'flex', flexFlow: 'column nowrap', alignItems: 'center', minHeight: '150px', maxWidth: '550px', width: '85%' }}>
       <Home />
     </div>
