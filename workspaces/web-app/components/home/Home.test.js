8a67db2380d62656abc01948e7c340f76addea96
RLPHH <ralph.holdorp@gmail.com>
2019-02-25 09:27:32 +0100

    added initial testing

---
+++ workspaces/web-app/components/home/Home.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import { Home } from './Home'
+
+describe('Home', () => {
+  it('renders correctly', () => {
+    const { container } = render(<Home />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('prints malloc somewhere', () => {
+    const { getByText } = render(<Home />)
+
+    expect(getByText(/malloc/i)).toBeInTheDocument()
+  })
+})
