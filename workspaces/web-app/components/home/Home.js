6ed75e5890a37dc1a1098f87c57917ec2a204075
RLPHH <ralph.holdorp@gmail.com>
2019-03-09 15:52:12 +0100

    added selectable table in filter

--- workspaces/web-app/components/home/Home.js
+++ workspaces/web-app/components/home/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FadingValueBox } from '../animations'
-import { Input, Menu, Segment } from 'semantic-ui-react'
+import { Table, Input, Menu, Segment } from 'semantic-ui-react'
 
 import { HomeGrid, HeaderGridItem, ActionsGridItem,
   ResultsGridItem, FooterGridItem } from './HomeGrid'
@@ -8,10 +8,36 @@ import { AddAssignment } from '../assignments'
 import { AddAllocation } from '../allocations'
 import { AddTeamMember } from '../team-members'
 
+const FilterTableRow = (props) => (
+  <Table.Row>
+    <Table.Cell>{props.name}</Table.Cell>
+    <Table.Cell>{props.contract}</Table.Cell>
+  </Table.Row>
+)
+
+const FilterTable = (props) => (
+  <Table selectable>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Team Member</Table.HeaderCell>
+        <Table.HeaderCell>Contract</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {props.children}
+    </Table.Body>
+  </Table>
+)
 const Home = () => {
   const [ activeItem, setActiveItem ] = useState('Team Members')
   const [ initialized, setInitialized ] = useState(false)
 
+  const team = [
+    { name: 'name00', contract: 'contract00' },
+    { name: 'name01', contract: 'contract01' },
+    { name: 'name02', contract: 'contract02' },
+    { name: 'name03', contract: 'contract03' }
+  ]
   useEffect(() => {
     if (!initialized) {
       console.log('Home component mounted')
@@ -33,7 +59,7 @@ const Home = () => {
         </ActionsGridItem>
 
         <ResultsGridItem>
-          <form>
+          <div>
             <Menu pointing >
               <Menu.Item
                 name='Team Members'
@@ -58,9 +84,15 @@ const Home = () => {
             </Menu>
 
             <Segment>
-              Filtered list...
+              <div>
+                <FilterTable>
+                  {team.map((member, index) => (
+                    <FilterTableRow key={index} {...member} />
+                  ))}
+                </FilterTable>
+              </div>
             </Segment>
-          </form>
+          </div>
         </ResultsGridItem>
         <FooterGridItem css={{ color: '#ff00cc' }}>
         © 2019 by Malloc
