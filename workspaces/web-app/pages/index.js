b1a9e9b5ce51634616a384a3577b55a46ba5b985
RLPHH <ralph.holdorp@gmail.com>
2019-07-22 13:06:44 +0200

    spanned layout over full screen

--- workspaces/web-app/pages/index.js
+++ workspaces/web-app/pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import { PageCentered } from '@react-frontend-developer/react-layout-helpers'
 import { Home } from '../components/home'
 
 const Index = () => (
-  <PageCentered>
-    <div css={{ display: 'flex', flexFlow: 'column nowrap', alignItems: 'center', minHeight: '150px', maxWidth: '800px', width: '85%' }}>
+    <div css={{ display: 'flex', flexFlow: 'column nowrap', minHeight: '150px' }}>
       <Home />
-    </div>
-  </PageCentered>
-)
+    </div>)
 
 export default Index
